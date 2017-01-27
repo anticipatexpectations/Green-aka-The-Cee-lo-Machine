8d962cbb13443a3aedec7f62e2cd17f77a13a35b
Hunter Lombard <hwlombard@gmail.com>
2017-01-27 14:08:03 -0500

    created score increase and loss conditions for p1

--- app.js
+++ app.js
@@ -1,72 +1,90 @@
-// var diceFace = {
-//   one:{
-//     value:1
-//     img:
-//   }
-//   two:{
-//     value:2
-//     img:
-//   }
-//   three:{
-//     value:3
-//     img:
-//   }
-//   four:{
-//     value:4
-//     img:
-//   }
-//   five:{
-//     value:5
-//   }
-//   six:{
-//     value:6
-//   }
-// }
-
-
 $diceArray = [
   {
-    img: 'http://i.imgur.com/keLQwA1.png'
+    img: 'img/diceImg1.png'
   },
   {
-    img: 'http://i.imgur.com/MxJDafx.png'
+    img: 'img/diceImg2.png'
   },
   {
-    img: 'http://i.imgur.com/K36HBvK.png'
+    img: 'img/diceImg3.png'
   },
   {
-    img: 'http://i.imgur.com/Q0cK4vZ.png'
+    img: 'img/diceImg4.png'
   },
   {
-    img: 'http://i.imgur.com/qQQQCIz.png'
+    img: 'img/diceImg5.png'
   },
   {
-    img: 'http://i.imgur.com/wq0NeTc.png'
+    img: 'img/diceImg6.png'
   }
 ]
-var $diceRando = $diceArray[Math.floor(Math.random()*($diceArray.length))].img;
+// var $diceRando = $diceArray[Math.floor(Math.random()*($diceArray.length))].img;
 
 $(document).ready(function(){
+  var score = 0;
+
+  var randomValue = function() {
+    return $diceArray[Math.floor(Math.random()*($diceArray.length))].img;
+  }
+
+  var $checkP1d1 = parseInt($('#diceP1-1').attr('src')[11]);
+  var $checkP1d2 = parseInt($('#diceP1-2').attr('src')[11])
+  var $checkP1d3 = parseInt($('#diceP1-3').attr('src')[11])
+  var $checkP2d1 = parseInt($('#diceP2-1').attr('src')[11])
+  var $checkP2d2 = parseInt($('#diceP2-2').attr('src')[11])
+  var $checkP2d3 = parseInt($('#diceP2-3').attr('src')[11])
+
+  var instaLose = function(){
+    var $diceRoll = [$checkP1d1, $checkP1d2, $checkP1d3];
+    $diceRoll.sort(function (a, b) {
+      return a-b
+    }); if (diceRoll[0]===1 && diceRoll[1]===2 && diceRoll[2]===3){
+      alert("you lose");
+    }
+  }
+  var $P1dubs = function(){
+    if ($checkP1d1 === $checkP1d2 || $checkP1d1 === $checkP1d3 || $checkP1d2 === $checkP1d3){
+      if ($checkP1d1 === 1 && $checkP1d2 === 1 || $checkP1d1 === 1 && $checkP1d13 === 1 || $checkP1d2 === 1 && $checkP1d3 === 1) {
+        $("P1score")=0;
+        alert("you lose");
+      } else {
+        score=score+5;
+      }
+    }
+  }
+  var $P1trips = function(){
+    if ($checkP1d1 === $checkP1d2 && $checkP1d2 === $checkP1d3) {
+      if ($checkP1d1 === 1) {
+        // snake eyes
+      } else {
+        score=score+10;
+      }
+    }
+  }
+  var $P1straight =function(){
+
+  }
 
   $("#rollBtn").on("click", function(){
     $(".dice").addClass("roll");
-    $("src").each(function(){
-      return $diceRando
-    });
 
-    // $("#diceP1-1").attr("src", $diceRando);
-    // $("#diceP1-2").attr("src", $diceRando);
-    // $("#diceP1-3").attr("src", $diceRando);
-    //
-    // $("#diceP2-1").attr("src", $diceRando);
-    // $("#diceP2-2").attr("src", $diceRando);
-    // $("#diceP2-3").attr("src", $diceRando);
+    $("#diceP1-1").attr("src", randomValue());
+    $("#diceP1-2").attr("src", randomValue());
+    $("#diceP1-3").attr("src", randomValue());
+
+    $("#diceP2-1").attr("src", randomValue());
+    $("#diceP2-2").attr("src", randomValue());
+    $("#diceP2-3").attr("src", randomValue());
+
+    if ($P1dubs > 1){
+      $P1dubs();
+    }
 
     $(".dice").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
     function(e) {
       $(".dice").removeClass("roll");
-
     });
+
   });
 
 });
