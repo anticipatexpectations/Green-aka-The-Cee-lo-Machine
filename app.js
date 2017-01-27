aaccea8bb68d2b753f67c9b71668714bd9bb251e
Hunter Lombard <hwlombard@gmail.com>
2017-01-27 17:47:40 -0500

    fully functional player 1 and finished jquery for player 2

--- app.js
+++ app.js
@@ -43,23 +43,20 @@ $(document).ready(function(){
     $checkP2d3 = parseInt($('#diceP2-3').attr('src')[11])
   };
 
-
-  var $instaLose = function(){
+  var $P1instaLose = function(){
     $getDiceValues();
     var $p1Dice = [
       $checkP1d1,
       $checkP1d2,
       $checkP1d3,
     ]
-    // var $diceRoll = [$checkP1d1, $checkP1d2, $checkP1d3];
-    // $diceRoll.sort(function (a, b) {
-    //   return a-b
 
     $p1Dice.sort(function (a, b) {
       return a-b
     }); if ($p1Dice[0]===1 && $p1Dice[1]===2 && $p1Dice[2]===3){
-      $("#P1score").text('$0');
-      alert("you lose");
+      $P1score = 0;
+      $("#P1score").text('$' + $P1score);
+      alert("Player 1 Loss");
     }
   }
   var $P1dubs = function(){
@@ -69,10 +66,12 @@ $(document).ready(function(){
 
     }
         else if ($checkP1d1 === 1 && $checkP1d2 === 1 || $checkP1d1 === 1 && $checkP1d3 === 1 || $checkP1d2 === 1 && $checkP1d3 === 1) {
-        $("#P1score").text('$'+ ($P1score=$P1score-$P1score));
-        alert("you lose");
+          $P1score = 0;
+          $("#P1score").text('$'+ $P1score);
+        // $("#P1score").text('$'+ ($P1score=$P1score-$P1score));
+        alert("Player 1 Loss");
       } else {
-        alert("You win: $5")
+        alert("Player 1 Win: $5")
         $P1score=$P1score+5;
         $("#P1score").text("$" + $P1score);
       }
@@ -81,13 +80,15 @@ $(document).ready(function(){
   var $P1trips = function(){
     $getDiceValues();
     if ($checkP1d1 === $checkP1d2 && $checkP1d2 === $checkP1d3) {
-      // if (){}
+
          if ($checkP1d1 === 1) {
-        $("#P1score").text('$' + ($P1score=$P1score-$P1score));
-        alert("you lose");
+        $P1score = 0;
+        $("#P1score").text("$" + $P1score);
+        // $("#P1score").text('$' + ($P1score=$P1score-$P1score));
+        alert("Player 1 Loss");
         // snake eyes
       } else {
-        alert("You Win: $10")
+        alert("Player 1 Win: $10")
         $P1score=$P1score+10;
         $("#P1score").text("$"+ $P1score);
       }
@@ -100,27 +101,118 @@ $(document).ready(function(){
       $checkP1d2,
       $checkP1d3,
     ]
-    // var $diceRoll = [$checkP1d1, $checkP1d2, $checkP1d3];
-    // $diceRoll.sort(function (a, b) {
-    //   return a-b
+
   $p1Dice.sort(function (a, b) {
       return a-b
     }); if ($p1Dice[0]===2 && $p1Dice[1]===3 && $p1Dice[2]===4){
-      alert("You Win: $15");
+      alert("Player 1 Win: $15");
       $P1score=$P1score+15;
       $("#P1score").text("$"+ $P1score);
     } else if ($p1Dice[0]===3 && $p1Dice[1]===4 && $p1Dice[2]===5){
-      alert("You Win: $15");
+      alert("Player 1 Win: $15");
       $P1score=$P1score+15;
       $("#P1score").text("$"+ $P1score);
     } else if ($p1Dice[0]===4 && $p1Dice[1]===5 && $p1Dice[2]===6){
-      alert("You Win: $30");
+      alert("Player 1 Win: $30");
       $P1score=$P1score+30;
       $("#P1score").text("$"+ $P1score);
     }
 
   }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+var $P2instaLose = function(){
+  $getDiceValues();
+  var $p2Dice = [
+    $checkP2d1,
+    $checkP2d2,
+    $checkP2d3,
+  ]
 
+  $p2Dice.sort(function (a, b) {
+    return a-b
+  }); if ($p2Dice[3]===1 && $p2Dice[4]===2 && $p2Dice[2]===3){
+    $P2score=0;
+    $("#P2score").text('$'+ $P2score);
+    alert("Player 2 Loss");
+  }
+}
+var $P2dubs = function(){
+  $getDiceValues();
+  if ($checkP2d1 === $checkP2d2 || $checkP2d1 === $checkP2d3 || $checkP2d2 === $checkP2d3){
+    if ($checkP2d1 === $checkP2d2 && $checkP2d2 === $checkP2d3){
+
+  }
+      else if ($checkP2d1 === 1 && $checkP2d2 === 1 || $checkP2d1 === 1 && $checkP2d3 === 1 || $checkP2d2 === 1 && $checkP2d3 === 1) {
+        $P2score = 0;
+        $("#P2score").text('$'+ $P2score);
+      // $("#P1score").text('$'+ ($P1score=$P1score-$P1score));
+      alert("Player 2 Loss");
+    } else {
+      alert("Player 2 Win: $5")
+      $P2score=$P2score+5;
+      $("#P2score").text("$" + $P2score);
+    }
+  }
+}
+var $P2trips = function(){
+  $getDiceValues();
+  if ($checkP2d1 === $checkP2d2 && $checkP2d2 === $checkP2d3) {
+
+       if ($checkP2d1 === 1) {
+      $P2score = 0;
+      $("#P1score").text("$" + $P2score);
+      // $("#P1score").text('$' + ($P1score=$P1score-$P1score));
+      alert("Player 2 Loss");
+      // snake eyes
+    } else {
+      alert("Player 2 Win: $10")
+      $P2score=$P2score+10;
+      $("#P1score").text("$"+ $P2score);
+    }
+  }
+}
+var $P2straight =function(){
+  $getDiceValues();
+  var $p2Dice = [
+    $checkP2d1,
+    $checkP2d2,
+    $checkP2d3,
+  ]
+
+$p2Dice.sort(function (a, b) {
+    return a-b
+  }); if ($p2Dice[0]===2 && $p2Dice[1]===3 && $p2Dice[2]===4){
+    alert("Player 2 Win: $15");
+    $P2score=$P2score+15;
+    $("#P2score").text("$"+ $P2score);
+  } else if ($p2Dice[0]===3 && $p2Dice[1]===4 && $p2Dice[2]===5){
+    alert("Player 2 Win: $15");
+    $P2score=$P2score+15;
+    $("#P2score").text("$"+ $P2score);
+  } else if ($p2Dice[0]===4 && $p2Dice[1]===5 && $p2Dice[2]===6){
+    alert("Player 2 Win: $30");
+    $P2score=$P2score+30;
+    $("#P2score").text("$"+ $P2score);
+  }
+
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
   $("#rollBtn").on("click", function(){
     $(".dice").addClass("roll");
 
@@ -136,10 +228,14 @@ $(document).ready(function(){
     function(e) {
       $(".dice").removeClass("roll");
     });
-    setTimeout($instaLose, 1000);
+    setTimeout($P1instaLose, 1000);
     setTimeout($P1dubs, 1000);
     setTimeout($P1trips, 1000);
     setTimeout($P1straight, 1000);
+    setTimeout($P2instaLose, 1000);
+    setTimeout($P2dubs, 1000);
+    setTimeout($P2trips, 1000);
+    setTimeout($P2straight, 1000);
 
   });
 
